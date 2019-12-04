8efe3f87389fdd7bb79653130bed2e3ac13f72e4
Jens Rudolf <jens.rudolf@gmx.net>
2019-12-04 21:28:38 +0100

    Improve indention and alignment to increase readability.

--- src/main/python/linuxband/mma/bar_info.py
+++ src/main/python/linuxband/mma/bar_info.py
@@ -140,11 +140,13 @@ class BarInfo:
 
     @staticmethod
     def create_event(eventTitle):
-        eventsInit = {Glob.A_GROOVE:        [Glob.A_GROOVE, "Groove", " ", "50sRock", "\n"],
-                      Glob.A_TEMPO:         [Glob.A_TEMPO, "Tempo", " ", "120", "\n"],
-                      Glob.A_REPEAT:        [Glob.A_REPEAT, "Repeat", "\n"],
-                      Glob.A_REPEAT_ENDING: [Glob.A_REPEAT_ENDING, "RepeatEnding", "\n"],
-                      Glob.A_REPEAT_END:    [Glob.A_REPEAT_END, "RepeatEnd", "\n"]}
+        eventsInit = {
+            Glob.A_GROOVE: [Glob.A_GROOVE, "Groove", " ", "50sRock", "\n"],
+            Glob.A_TEMPO:  [Glob.A_TEMPO, "Tempo", " ", "120", "\n"],
+            Glob.A_REPEAT: [Glob.A_REPEAT, "Repeat", "\n"],
+            Glob.A_REPEAT_ENDING: [Glob.A_REPEAT_ENDING, "RepeatEnding", "\n"],
+            Glob.A_REPEAT_END:    [Glob.A_REPEAT_END, "RepeatEnd", "\n"]
+        }
         return copy.deepcopy(eventsInit[eventTitle])
 
     @staticmethod
